7f8d4e82c0eb4a8f003d16b744e62bbcfe52ee71
Tinnaphop <tinnaphop.t@st.econ.tu.ac.th>
2024-05-21 23:23:16 +0700

    take assignment

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,7 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
+let fahrenheit = (celsius * 1.8) + 32
+console.log((celsius x 1.8) + 32);
+console.log(fahrenheit);
 
-// Start coding here;
-
-let fahrenheit;
+let fahrenheit = 86;
